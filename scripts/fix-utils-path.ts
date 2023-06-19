bf9b407a08c2e0ee914617d6506d033cbb10f9ef
laoer536 <1547698569@qq.com>
2023-06-19 19:16:39 +0800

    fix: fix-utils-path

--- scripts/fix-utils-path.ts
+++ scripts/fix-utils-path.ts
@@ -4,6 +4,7 @@ import { readFileSync, writeFileSync } from 'node:fs'
 
 const filename = fileURLToPath(import.meta.url)
 const nodeBinRoot = join(filename, '../../')
+console.log(join(nodeBinRoot, './dist/self-bin/create-bin.mjs'))
 const createBinCode = readFileSync(join(nodeBinRoot, './dist/self-bin/create-bin.mjs'), 'utf-8')
-createBinCode.replace('../utils', '../utils/index.mjs')
-writeFileSync(join(nodeBinRoot, './dist/self-bin/create-bin.mjs'), createBinCode)
+const fixCode = createBinCode.replace('../utils', '../utils/index.mjs')
+writeFileSync(join(nodeBinRoot, './dist/self-bin/create-bin.mjs'), fixCode)
