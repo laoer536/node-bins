64755685b23f88f2643f1025148c6015f40ae3d6
laoer536 <1547698569@qq.com>
2023-06-20 22:26:35 +0800

    feat: delete force-push

---
+++ self-bin/create-bin.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+// import minimist from 'minimist'
+import { red, lightGreen, lightYellow } from 'kolorist'
+import { fileURLToPath } from 'node:url'
+import { join, dirname } from 'pathe'
+import { cwd } from 'node:process'
+import consola from 'consola'
+import { readFileSync, writeFileSync, renameSync } from 'node:fs'
+import { execaCommand } from 'execa'
+import { getPackageManager, getFormatCode } from '../utils'
+
+const filename = fileURLToPath(import.meta.url)
+const nodeBinRoot = join(filename, '../../..')
+const buildUserBinRoot = join(nodeBinRoot, './build-user-bin')
+const userBinRoot = join(nodeBinRoot, './dist/user-bin')
+const userRoot = cwd()
+const packageManager = getPackageManager()
+
+const isInCommandFileDir = await consola.prompt(
+  `When you execute the 'create-bin' command, is it in the folder corresponding to the source file?`,
+  {
+    type: 'confirm',
+  }
+)
+
+if (isInCommandFileDir) {
+  createBin()
+    .then(({ binName }) => {
+      const logBinColored = lightYellow(binName)
+      consola.success(
+        lightGreen(
+          `You have successfully created the command ${logBinColored},Try typing in the terminal ${logBinColored}, execute the command you created，Or your command involves passing in parameters，of course you can also add your parameters later.`
+        )
+      )
+    })
+    .catch((err) => {
+      consola.error(new Error(`${err}`))
+    })
+} else {
+  consola.error(
+    red(
+      `When executing the 'create-bin' command, make sure that the current path is in the folder corresponding to the source file`
+    )
+  )
+}
+
+async function createBin() {
+  const binName = await consola.prompt('binName', { type: 'text', placeholder: '请输入您将创建的command名称' })
+  const binFileName = await consola.prompt('binFile', {
+    type: 'text',
+    placeholder: `Please enter the name of the js file corresponding to this command (need to include the suffix, and be ".js" or ".ts")`,
+  })
+  consola.info('Start creating global commands based on your source files....')
+
+  /** build user bin's code **/
+  const binFileCode = readFileSync(`${userRoot}/${binFileName}`, 'utf-8')
+  writeFileSync(join(buildUserBinRoot, './src/index.ts'), binFileCode)
+  const { stdout } = await execaCommand(`${packageManager} run build`, { cwd: buildUserBinRoot })
+  console.log(stdout)
+  renameSync(`${buildUserBinRoot}/bin/index.mjs`, `${buildUserBinRoot}/bin/${binFileName}`)
+
+  /** create user's bin **/
+  const packageJson = JSON.parse(readFileSync(join(buildUserBinRoot, './package.json'), 'utf-8'))
+  packageJson.bin[binName] = `./bin/` + binFileName
+  const binedPackagesJsonStr = await getFormatCode(JSON.stringify(packageJson, null, 2), { parser: 'json' })
+  writeFileSync(join(buildUserBinRoot, './package.json'), binedPackagesJsonStr)
+
+  /** link bin **/
+  const { stdout: linkStdout } = await execaCommand(`${packageManager} run link`, { cwd: buildUserBinRoot })
+  return { binName }
+}
