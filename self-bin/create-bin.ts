04c714d29abd5a72090b90676865a4005acd9d2b
laoer536 <1547698569@qq.com>
2023-06-20 19:38:07 +0800

    fix: fix user-node-bin cant see

--- self-bin/create-bin.ts
+++ self-bin/create-bin.ts
@@ -59,7 +59,7 @@ async function createBin() {
   writeFileSync(join(nodeBinRoot, './package.json'), binedPackagesJsonStr)
 
   /** link bin **/
-  const { stdout: linkStdout } = await execaCommand(`npm link`, { execPath: nodeBinRoot })
+  const { stdout: linkStdout } = await execaCommand(`npm link`, { cwd: nodeBinRoot })
   console.log(linkStdout)
   return { binName }
 }
